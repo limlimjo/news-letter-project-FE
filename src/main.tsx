e182d0cdd78de1dbb66dfa427d25708b774de589
limlimjo <skyblue.777@daum.net>
2025-10-02 14:28:42 +0000

    feat: 라우팅 구조 세팅하기 #4

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./pages"; // 메인 페이지 컴포넌트
+import RootLayout from "./pages/layout"; // 레이아웃 컴포넌트
+import ContentsList from "./pages/contents/ContentsList";
+import ContentsDetail from "./pages/contents/ContentsDetail";
+import NewSubscriber from "./pages/subscribers/NewSubscriber";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<RootLayout />}>
+          <Route index element={<App />} />
+          <Route path="/contents" element={<ContentsList />} />
+          <Route path="/contents/:id" element={<ContentsDetail />} />
+          <Route path="/subscribers/new" element={<NewSubscriber />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
