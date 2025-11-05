7d44942cba1f9e3af89ff5dcabe8f6bcc158e2b2
limlimjo <skyblue.777@daum.net>
2025-11-05 14:22:23 +0000

    chore: vercel 오류 해결

--- src/pages/contents/ContentsList.tsx
+++ src/pages/contents/ContentsList.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import Button from "@/components/ui/button";
+import Button from "@/components/ui/Button";
 import CardContent from "@/components/ui/CardContent";
 import CommonInput from "@/components/ui/CommonInput";
 
