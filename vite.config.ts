06ae69de5eca3a104865b30df1a6f27192202e6e
limlimjo <skyblue.777@daum.net>
2025-12-27 09:04:40 +0000

    feat: 백엔드 API 연결 - 구독하기 #17

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,29 @@
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
-//import { SERVER_URL } from "./src/config/index";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  // mode에 맞는 .env 파일을 로드
+  const env = loadEnv(mode, process.cwd());
+  const SERVER_URL = env.VITE_API_URL;
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: `${SERVER_URL}`,
-  //       changeOrigin: true,
-  //       rewrite: path => path.replace(/^\/api/, ''),
-  //     },
-  //   },
-  // },
+    server: {
+      proxy: {
+        "/api": {
+          target: SERVER_URL,
+          changeOrigin: true,
+          rewrite: (p) => p.replace(/^\/api/, ""),
+        },
+      },
+    },
+  };
 });
